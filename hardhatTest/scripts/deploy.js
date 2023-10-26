04e73dcca6a2f44fa1efdac40a06177dbc2bcadc
gitStevenChen <stevenchen_official@outlook.com>
2023-10-26 17:03:12 +0800

    新建hardhat

--- hardhatTest/scripts/deploy.js
+++ hardhatTest/scripts/deploy.js
@@ -7,17 +7,23 @@
 const hre = require("hardhat");
 
 async function main() {
-  const PriceConsumerV3 = await hre.ethers.getContractFactory("PriceConsumerV3");
-  const priceConsumerV3 = await PriceConsumerV3.deploy();
-  await priceConsumerV3.deployed();
-  console.log("PriceConsumerV3 deployed to: ", PriceConsumerV3.address);
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+  const lockedAmount = hre.ethers.utils.parseEther("1");
+
+  const Lock = await hre.ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+  await lock.deployed();
+
+  console.log("Lock with 1 ETH deployed to:", lock.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
 });
--- myCompound/README.md
+++
@@ -1,8 +0,0 @@
-# compound简化版
-自己写的compound简化版（参考v2版本），使用openzeppelin的合约模板以及Hardhat开发环境，目前只有ctoken核心合约、利率模型合约、审计合约和价格预言机，部署在sepolia测试网上。（不要部署在goerli，很难部署上去）
-
-- 利率模型合约直接采用compound源码的直线型，WhitePaperInterestRateModel。
-- 价格预言机使用chainlink的方法来实现PriceOracle，新写一个PriceOracle实现类ChainlinkPriceOracle。
-- 审计合约的逻辑合约采用最早的G1版本ComptrollerG1，代理合约用Unitroller
-- 核心合约
-
