ef7a9037a04396f20aea4246569319ce7088498e
gitStevenChen <stevenchen_official@outlook.com>
2023-12-07 23:03:11 +0800

    使用OpenZeppelin Test Helpers增加测试功能

--- hardhatTest/test/Test.js
+++ hardhatTest/test/Test.js
@@ -1,6 +1,12 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 require("dotenv").config();
+const {
+    BN,           // Big Number support
+    constants,    // Common constants, like the zero address and largest integers
+    expectEvent,  // Assertions for emitted events
+    expectRevert, // Assertions for transactions that should fail
+  } = require('@openzeppelin/test-helpers');
 
 let transparentProxy
 
